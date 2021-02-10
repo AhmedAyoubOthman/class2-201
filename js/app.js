58ba2f7d7667ad30c837fab99be2968f0aa19db8
mohammad ashour <moh.ash96@gmail.com>
2021-02-10 15:24:48 +0200

    1st q

--- js/app.js
+++ js/app.js
@@ -4,24 +4,28 @@ let userName = prompt('Hello, What\'s your name?')
 console.log(userName);
 alert('Hi ' + userName + ", let's have fun with playing a guessing game!")
 
-let height = prompt('Am I tall?');
-switch (height.toUpperCase()) {
-    case 'YES':
-    case 'Y':
-        //console.log('Sorry, You\'re incorrect.');
-        alert('Sorry, You\'re incorrect');
-        break;
-    case 'NO':
-    case 'N':
-        //console.log('You\re correct.');
-        score++;
-        alert('You\'re correct.');
-        break;
-    default:
-        //console.log('Please write just yes or no!');
-        alert('Please write just yes or no!');
-
+function firstQ(params) {
+    
+    let height = prompt('Am I tall?');
+    switch (height.toUpperCase()) {
+        case 'YES':
+        case 'Y':
+            //console.log('Sorry, You\'re incorrect.');
+            alert('Sorry, You\'re incorrect');
+            break;
+        case 'NO':
+        case 'N':
+            //console.log('You\re correct.');
+            score++;
+            alert('You\'re correct.');
+            break;
+        default:
+            //console.log('Please write just yes or no!');
+            alert('Please write just yes or no!');
+    
+    }
 }
+firstQ();
 
 let Favanimal = prompt('Am I love cats?');
 switch (Favanimal.toUpperCase()) {
